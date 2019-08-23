c770e8b7b7207a3549e50064bc4a1d3ff8d4b89d
zhangjing <juslly@163.com>
2019-08-23 09:47:08 +0800

    practice 2

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,15 @@
 package practice02;
 
-public class Student {
+public class Student extends Person{
+	
+	private String klass;
+	public Student(String name, int age, String klass) {
+		super(name, age);
+		this.klass = klass;
+		// TODO Auto-generated constructor stub
+	}
+	public String introduce() {
+		return " I am a Student. I am at Class "+ klass;
+		
+	}
 }
